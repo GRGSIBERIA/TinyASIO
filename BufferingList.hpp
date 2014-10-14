ddc897741d372e021178350983ca346a94343a85
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-10-14 17:10:53 +0900

    たくさん作ったけどいらないって思った

--- BufferingList.hpp
+++ BufferingList.hpp
@@ -7,7 +7,7 @@ namespace asio
 	* バッファリング用のリスト
 	* @tparam T バッファの型
 	*/
-	template <typename T = void*>
+	template <typename T>
 	class BufferingList
 	{
 		std::vector<T> buffer;
@@ -19,49 +19,16 @@ namespace asio
 
 		}
 
-		void Push(const BufferingList<T>& target)
+		void Store(const BufferingList<T>& target)
 		{
 			buffer.insert(buffer.end(), target.buffer.begin(), target.buffer.end());
 		}
-	};
 
-	/**
-	* Int型のバッファリング用のリスト
-	*/
-	class IntBufferingList : BufferingList<int>
-	{
-	public:
-		IntBufferingList(const void* buffer, const long size)
-			: BufferingList(buffer, size) {}
-	};
-
-	/**
-	* Short型のバッファリング用のリスト
-	*/
-	class ShortBufferingList : BufferingList<short>
-	{
-	public:
-		ShortBufferingList(const void* buffer, const long size)
-			: BufferingList(buffer, size) {}
-	};
-
-	/**
-	* Float型のバッファリング用のリスト
-	*/
-	class FloatBufferingList : BufferingList<float>
-	{
-	public:
-		FloatBufferingList(const void* buffer, const long size)
-			: BufferingList(buffer, size) {}
-	};
-
-	/**
-	* Double型のバッファリング用のリスト
-	*/
-	class DoubleBufferingList : BufferingList<double>
-	{
-	public:
-		DoubleBufferingList(const void* buffer, const long size)
-			: BufferingList(buffer, size) {}
+		std::vector<T> Spend()
+		{
+			auto buf = std::vector<T>(buffer.begin(), buffer.end());
+			buffer.clear();
+			return buf;
+		}
 	};
 }
