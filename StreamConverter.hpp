97969e2e84ae3c432e514f2663ba688ffdd4e4e3
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-10-16 19:13:26 +0900

    bufferとsource配列の型変換と値の丸めを行うクラスを作る

--- StreamConverter.hpp
+++ StreamConverter.hpp
@@ -1,33 +1,117 @@
 #pragma once
 #include <vector>
+#include <exception>
+#include <typeinfo>
 #include "Option.hpp"
 
 namespace asio
 {
 	namespace conv
 	{
-		void IntToOptionType(std::vector<TINY_ASIO_BUFFER_TYPE>& source, void* buffer, const long size)
+		class UnknownOptionType : public std::exception
 		{
-			const long count = size / sizeof(int);
+		public:
+			UnknownOptionType(const std::string& message)
+				: exception(message.c_str()) { }
+		};
 
-#if TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_INT
-			source.insert(source.end(), reinterpret_cast<int*>(buffer), reinterpret_cast<int*>(buffer) + count);
-#elif TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_FLOAT
-			
-			const long prevSize = source.size();
-			source.resize(prevSize + count);
-
-			const float MAX_DIFF = 1.0 / 2147483647.0;
-			for (long i = 0; i < count; ++i)
+		/**
+		* bufferからsourceへ流し込む処理のためのクラス
+		* 適宜，型の変換処理も行っている
+		*/
+		class BufferPourer
+		{
+			static const long ResizeSource(std::vector<TINY_ASIO_BUFFER_TYPE>& source, const long count)
 			{
-				source[prevSize + i] = *(reinterpret_cast<int*>(buffer)+i) * MAX_DIFF;
+				const long prevSize = source.size();
+				source.resize(prevSize + count);
+				return prevSize;
 			}
-#endif
-		}
 
-		void FloatToOptionType(std::vector<TINY_ASIO_BUFFER_TYPE>& source, void* buffer, const long size)
-		{
-			const long count = size / sizeof(float);
-		}
+			/**
+			* オプションの型と
+			*/
+			static const float DecideMaxDiffFromType(const type_info& type)
+			{
+				float MAX_DIFF;
+
+				if (type == typeid(int))
+#if TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_INT
+					MAX_DIFF = 1.0;
+#elif TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_FLOAT
+					MAX_DIFF = 1.0 / 2147483647.0;
+#endif
+				else if (type == typeid(short))
+#if TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_INT
+					MAX_DIFF = 65538.0;
+#elif TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_FLOAT
+					MAX_DIFF = 1.0 / 32767.0;
+#endif
+				else if (type == typeid(float) || type == typeid(double))
+#if TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_INT
+					MAX_DIFF = 2147483647.0;
+#elif TINY_ASIO_BUFFER_OPTION == TINY_ASIO_BUFFER_FLOAT
+					MAX_DIFF = 1.0;
+#endif
+				else
+					throw UnknownOptionType("判別できないバッファの型が指定されています, see also Option.hpp");
+
+				return MAX_DIFF;
+			}
+
+			template <typename T>
+			static void PourIntoSource(std::vector<TINY_ASIO_BUFFER_TYPE>& source, void* buffer, const long count)
+			{
+				const long prevSize = ResizeSource(source, count);
+				const float MAX_DIFF = DecideMaxDiffFromType<T>();
+
+				for (long i = 0; i < count; ++i)
+					source[prevSize + i] = *(reinterpret_cast<T*>(buffer)+i) * MAX_DIFF;
+			}
+
+			/**
+			* 1つの値のエンディアンを反転させる
+			*/
+			template <typename T>
+			static void ReverseEndian(T* p)
+			{
+				std::reverse(
+					reinterpret_cast<BYTE*>(p),
+					reinterpret_cast<BYTE*>(p)+sizeof(T));
+			}
+
+		public:
+			/**
+			* bufferからsourceへ流し込む処理
+			*/
+			template <typename T>
+			static void ConvertToOptionType(std::vector<TINY_ASIO_BUFFER_TYPE>& source, void* buffer, const long size)
+			{
+				const long count = size / sizeof(T);
+
+				if (typeid(TINY_ASIO_BUFFER_TYPE) == typeid(int) || typeid(TINY_ASIO_BUFFER_TYPE) == typeid(float))
+				{
+					source.insert(source.end(), reinterpret_cast<T*>(buffer), reinterpret_cast<T*>(buffer)+count);
+				}
+				else
+				{
+					PourIntoSource<T>(source, buffer, count);
+				}
+			}
+
+			/**
+			* 受け取ったバッファのエンディアンを反転させる
+			*/
+			template <typename T>
+			static void FormatBigEndian(void* buffer, const long size, const long typeSize = sizeof(T))
+			{
+				T *start = reinterpret_cast<T*>(buffer);
+				const size_t num = size / sizeof(T);
+				for (size_t i = 0; i < num; ++i)
+				{
+					ReverseEndian(start + i * typeSize);
+				}
+			}
+		};
 	}
 }
