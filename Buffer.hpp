22fdc3ac65f69277b5f9a1a75e247722d7fa27dc
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-11-11 21:54:05 +0900

    バッファ用のクラスを作る

--- Buffer.hpp
+++ Buffer.hpp
@@ -1,18 +1,26 @@
 #pragma once
 #include <vector>
+#include <Windows.h>
 
 #include "SDK.hpp"
 #include "Driver.hpp"
 
 namespace asio
 {
+	typedef std::shared_ptr<std::vector<int>> StreamingBuffer;
+
 	/**
 	* バッファ用のクラス
 	*/
 	class BufferBase
 	{
-		void *buffers[2];
-		long channelNumber;
+	protected:
+		void *buffers[2];	//!< バッファ
+		long channelNumber;	//!< チャンネル番号
+
+		StreamingBuffer stream;		//!< ストリーミング用の変数
+		CRITICAL_SECTION critical;	//!< クリティカルセクション
+
 
 	public:
 		BufferBase(const ASIOBufferInfo& info)
@@ -20,10 +28,52 @@ namespace asio
 		{
 			buffers[0] = info.buffers[0];
 			buffers[1] = info.buffers[1];
+
+			stream = StreamingBuffer(new std::vector<int>());
+			InitializeCriticalSection(&critical);
+		}
+
+		
+		virtual ~BufferBase()
+		{
+			DeleteCriticalSection(&critical);
+		}
+
+
+		inline const long ChannelNumber() const { return channelNumber; }	//!< チャンネル番号
+		inline void* GetBuffer(const long index) { return buffers[index]; }	//!< indexからバッファを取得する
+
+
+		/**
+		* バッファの中身を取り出す
+		* @return バッファの中身
+		*/
+		StreamingBuffer Fetch()
+		{
+			StreamingBuffer retval = stream;
+			EnterCriticalSection(&critical);
+			stream = StreamingBuffer(new std::vector<int>());
+			LeaveCriticalSection(&critical);
+			return retval;
+		}
+
+
+		/**
+		* バッファに値を蓄積する
+		* @param[in] store 蓄積したい値
+		*/
+		void Store(const std::vector<int>& store)
+		{
+			EnterCriticalSection(&critical);
+			stream->insert(stream->end(), store.begin(), store.end());
+			LeaveCriticalSection(&critical);
 		}
 	};
 
 
+	/**
+	* 入力バッファ, ギターやマイクなどの入力を扱う
+	*/
 	class InputBuffer : public BufferBase
 	{
 	public:
@@ -32,6 +82,9 @@ namespace asio
 	};
 
 
+	/**
+	* 出力バッファ，ヘッドフォンやスピーカーなどへ出力する
+	*/
 	class OutputBuffer : public BufferBase
 	{
 	public:
