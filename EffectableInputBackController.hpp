58c53d39b1bee171b8f1a664da5526dd4440182f
Eiichi Takebuchi <nanashi4129@gmail.com>
2014-11-23 16:03:36 +0900

    GPLの条文を追加

--- EffectableInputBackController.hpp
+++ EffectableInputBackController.hpp
@@ -1,3 +1,20 @@
+/***********************************************************************
+Copyright(C) 2014  Eiichi Takebuchi
+
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>
+***********************************************************************/
+
 #pragma once
 #include "ControllerBase.hpp"
 
@@ -6,6 +23,7 @@ namespace asio
 	/*
 	* エフェクター付きのコントローラ
 	* 入力バッファとバッファ長を受け取るラムダ式をコールバック関数内で実行する
+	* 動くかどうかはわからない
 	* @tparam EFFECT_FUNC void (*)(void*, long)なラムダ式
 	*/
 	template <typename EFFECT_FUNC>
